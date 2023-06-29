994096e0f7340a79b4e6392bf3eb34667c5ecfc1
Thanusha987 <thanusha.xworkz@gmail.com>
2023-06-29 12:50:49 +0530

    Initial

---
+++ Constructor/Country.java
@@ -0,0 +1,23 @@
+class Country{
+	Country()
+	{
+		System.out.println("India has rich culture");
+	}
+}
+class CountryRunner{
+	public static void main(String[] args)
+	{
+		Country country=new Country();
+		Country states=new Country();
+		Country people=new Country();
+		Country capital=new Country();
+		Country population=new Country();
+		Country roads=new Country();
+		Country hospitals=new Country();
+		Country area=new Country();
+		Country primeMinister=new Country();
+		Country sports=new Country();
+		Country politics=new Country();
+	}
+}
+		
---
+++ Constructor/Movie.java
@@ -0,0 +1,23 @@
+class Movie{
+	Movie()
+	{
+		System.out.println("Let's watch a movie today");
+	}
+}
+class MovieRunner{
+	public static void main(String[] args)
+	{
+		Movie movie=new Movie();
+		Movie award=new Movie();
+		Movie hero=new Movie();
+		Movie heroine=new Movie();
+		Movie musicDirector=new Movie();
+		Movie producer=new Movie();
+		Movie oscarAward=new Movie();
+		Movie comedian=new Movie();
+		Movie songs=new Movie();
+		Movie filmFareAward=new Movie();
+		Movie budget=new Movie();
+	}
+}
+		
