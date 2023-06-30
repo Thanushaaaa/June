843b7b6f2881d25c4df8d432aab907bd3c7367d0
Thanushaaaa <thanushapamudurthi@gmail.com>
2023-06-30 13:49:27 +0530

    test

---
+++ Test.java
@@ -0,0 +1,86 @@
+
+1.a-29
+2.c-20
+3.c-96
+4.b-62
+5.b
+6.b-k
+7.a-p
+8.b-FR
+9.c-bccab
+10.a-GOV
+11-AC,BE,DF,HJ
+
+
+
+1.Ans-javac is used for compiling java files.It is present program/java/bin folder.
+It converts .java file into .class file
+java is used for running java files.It takes .class file as input.It is present in JRE
+
+2.Ans-JDK-JDK stands for Java Development Kit.It contains JRE,JVM,tools and libraries/
+JRE-Java Runtime Environment.JRE has JVM.JRE runs the code
+JVM-Java Virtual Machine-JVM is in JRE.JVM is the one who actually executes the code.
+
+3.Ans-Limitations of array:
+1.Only same type of values should be stored in array
+2.Array elements are accessed by using the index value of the elements
+3.Array index value starts from 0
+4.Data type for the array should be declared during its creation
+
+4.Ans-Class-class is the blueprint/design that an object follows
+      Object-object is the real world entity
+	  class class_name{ }
+	  
+5.Ans-Array-Array is like a container that stores same kind of values in it
+      Ex-int[] numbers={3,2,5,7,1};
+	  
+6.Ans-Difference between .java file and .class file:
+1..java file is in human language that is understood only by the humans..java file is given as input to the JRE
+   whereas .class file is understood by the machines.It can be run on any platform once it is generated.
+   .class file makes java as platform independent
+   
+7.Ans-Length of given array is 6
+
+8.Ans-non-access specifier return_type method_name()
+
+9.Ans-Java is an object-oriented,platform independent programming language.
+      It follows the concept of Write Once Run Anywhere.
+	  
+10.Ans-variable-variable stores the value.It acts like reference to that value in the memory.
+       Types of varibles are:
+	   1.Instance variable and class variable(or)
+	   2.non-static variable and static variable
+	   
+11.Ans-A class variable has the 
+
+12.Ans-data type shows what kind of data is stored
+       Types of data types
+	   1.primitive(short,byte,int,char,boolean,long,double,string)
+	   2.non-primitive(class)
+	   
+13.Ans-Java features:
+     1.Security
+	 2.Object oriented
+	 3.Platform independent
+	 4.Robust
+	 
+	 
+14.Ans-Types of array-int,string,long,byte,short.
+
+15.Ans-array index out of bound exception arises when we try to access the element in the index that is 
+    not present in the array
+	
+16.System.out.println is the statement written to print the output on the console
+   System-predefined class
+   dot operator
+   out-static variable
+   println-static method
+
+17.double equal operator is used to check whether two values are equal or not
+
+18.dot operator is used to access the members of the class
+
+19.The class which contains main method is the base class of all the classes
+
+20.10 java keywords:
+      class,extends,implements,package,private,static,void,public,new,return
