c7e9234c1e4570f6a094acdec084c59a6ef10527
Thanushaaaa <thanushapamudurthi@gmail.com>
2023-07-08 13:22:53 +0530

    java test

---
+++ 2nd_test/Test2 and Aptitude2.java
@@ -0,0 +1,187 @@
+1.d.Maternal uncle
+2.b.Grandfather
+3.a.Brother
+4.d.Brother
+5.c.Grandson
+6.c. X+A*Y
+7.c.R
+8.Brother
+9.a.father
+10.b.R
+11.a.10km
+12.b.South
+13.a.South West
+14.c.South West
+
+
+Java
+
+1.Error:incompatible types
+
+2.Output-10,110
+
+3.Error:incompatible types
+
+4.Java starts its execution from main method.Java runs only the main method which has String array 
+as arguments.
+
+5.To reverse array elements
+input {10,45,7,9,2}
+output {2,9,7,45,10}
+
+class ReverseArrayElements
+{
+	public static void main(String[] args)
+	{
+	int[] array1={10,45,7,9,2};
+	int[] array2={}
+	for(index=0;index<array1.length;index++)
+	{
+	int array2[index]=array1[index];
+	System.out.println(array2.add(array1[index]);
+	}
+	System.out.println(array2);
+}
+}
+
+
+
+6.Input:{2,5,4,8,2,3,6,2}
+Output:{8,6,5,4,3,2,1}
+
+class Arrays{
+	public static void main(String[] args)
+	{
+		int[] array1={2,5,4,8,2,3,6,2};
+		int[] array2={};
+		for(index=array1.length;index>=0;index--)
+		{
+		if(array1[index]>array1[index]
+			
+
+
+
+
+7.Output : Done
+
+
+
+8.Difference between this and this():
+     1.this is a keyword in java used to reference the instance variables.The variables when written with
+	 this keyword has memory in instance pool.this keyword is used to differentiate between instance variable
+	 and local variable.
+	 
+	 this() is used for constructor chaining.this() is used to call one constructor from another constructor
+	 within the same class.this() should be the first statement in the method during constructor chaining.
+	 
+	 Example of this:
+	 
+	 class Example{
+		 int a
+			 
+			 Example()
+			 {
+				 this.a=a;
+				 System.out.println("hello");
+			 }
+	 }
+	 class ExampleRunner{
+		 {
+			 public static void main(String[] args)
+			 {
+				 Example example=new Example(10);
+				 System.out.println(example);
+			 }
+		 }
+		
+		
+    Example of this():
+	
+	    class Example2{
+			String name;
+			int age;
+			Example2()
+			{
+				this.name=name;
+				this.age=age;
+				System.out.println(name);
+				System.out.println(age);
+				
+			}
+			Example2(String gender)
+			{
+				this(name,age);
+				this.gender=gender;
+				System.out.println(gender);
+			}
+			
+	class Example2Runner{
+		public static void main(String[] args){
+			Example example2=new Example("Neha",45,"female");
+				 System.out.println(example2);
+		}
+	}
+			
+		
+				
+			 
+	 
+	 
+9.class Swap
+{
+	public static void main(String[] args)
+	{
+	int a=10;
+	int b=20;
+	int a=b;
+	System.out.println(a);
+	int a=10;
+	int b=a;
+	System.out.println(b);
+	}
+}
+
+
+
+10.1.class Pattern{
+	public static void main(String[] args)
+	{
+		System.out.println("& "+"  "+" &");
+		System.out.println(" "+"  "+" ");
+		System.out.println(" "+" & "+" ");
+		System.out.println(" "+"  "+" ");
+		System.out.println("& "+"  "+" &");
+	}
+}
+	
+
+   2.class Pattern1{
+	  public static void main(String[] args)
+	{
+		System.out.println("% "+"%"+" %");
+		System.out.println(" "+"  "+" ");
+		System.out.println("% "+"&"+" %");
+		System.out.println(" "+"  "+" ");
+		System.out.println("% "+"%"+" %");
+	}
+}	
+
+
+  3.class Pattern2{
+	   public static void main(String[] args)
+	{
+		System.out.println("% "+"%"+" %");
+		System.out.println(" "+"  "+" ");
+		System.out.println("% "+" "+" %");
+		System.out.println(" "+"  "+" ");
+		System.out.println("% "+"%"+" %");
+	}
+}	
+
+
+
+
+	
+	
+	
+	 
